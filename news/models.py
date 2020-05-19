1c6003a16c403c7b80cff2d80076ff388a16eda0
Trinity John <trinityrtime@gmail.com>
2020-05-19 22:40:20 +0300

    ordering queries

--- news/models.py
+++ news/models.py
@@ -8,3 +8,9 @@ class Editor(models.Model):
 
     def __str__(self):
         return self.first_name
+
+    try:
+        editor = Editor.objects.get(email = 'example@gmail.com')
+        print('Editor found')
+    except DoesNotExist:
+        print('Editor was not found')
